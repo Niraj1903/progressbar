bdaa33c4589a052eacaf53715e581b9f7e235fdd
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-03 23:48:25 +0530

    multiple progress bar created

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import ProgressBar from "./ProgressBar";
 import "./App.css";
 
 function App() {
+  const bars = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95];
   return (
     <>
       <h1 className="font-bold flex justify-center mt-2">ProgressBar</h1>
-      <ProgressBar progress={50} />
+      {bars.map((item) => (
+        <ProgressBar key={item} progress={item} />
+      ))}
     </>
   );
 }
