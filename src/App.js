920f12cf5df97431d232382cabe332f70bf0a7dd
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-04 00:15:54 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ProgressBar from "./ProgressBar";
 import "./App.css";
 
 function App() {
-  const bars = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95];
+  const bars = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
   return (
     <>
       <h1 className="font-bold flex justify-center mt-2">ProgressBar</h1>
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,12 +1,21 @@
 const ProgressBar = (props) => {
   return (
     <>
-      <div className=" mx-2 ">
+      <div className="mx-2 my-2 h-8 rounded-lg bg-gray-200 shadow-inner overflow-hidden">
         <div
-          className="bg-green-800 mb-1 border border-black rounded-lg text-white text-center"
-          style={{ width: `${props.progress}%` }}
+          className="h-full rounded-lg border border-gray-300 relative"
+          style={{
+            width: `${props.progress}%`,
+            backgroundColor: "#34D399",
+            color: props.progress > 5 ? "white" : "black",
+            backgroundImage:
+              "repeating-linear-gradient(45deg, rgba(255,255,255,0.15) 0, rgba(255,255,255,0.15) 10px, transparent 10px, transparent 20px)",
+            transition: "width 1s ease-in-out",
+          }}
         >
-          {props.progress}%
+          <span className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-semibold text-white select-none">
+            {props.progress}%
+          </span>
         </div>
       </div>
     </>
